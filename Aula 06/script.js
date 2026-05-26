07d6c5563b8adb2c16aa0f57a69e5e256f00aae6
Gustavo1208a <gualquieri@gmail.com>
2026-05-26 09:10:42 -0300

    Aula 06

---
+++ Aula 06/script.js
@@ -0,0 +1,12 @@
+let frutas = [ "Maça", "Uva", "Pera"]
+
+console.log(frutas, frutas.length)
+
+let aluno = {
+    id: 1,
+    nome: "Lambido",
+    email: "teste@email.com",
+    senha: 123
+}
+
+console.log(aluno)
