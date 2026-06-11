53be09e985a0f1a28f9105bce5ea38a7e064dde7
gustavo1208a <gualquieri@gmail.com>
2026-06-11 09:10:33 -0300

    Aula 09

--- Aula 08/script.js
+++ Aula 08/script.js
@@ -1,9 +1,15 @@
-const spanResult = document.querySelector("#resultado")
+let form = document.querySelector("#formulario")
+const result = document.querySelector("#resultado")
 
-function showResult() {
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
     const nome = document.querySelector("#nome").value;
     const sobrenome = document.querySelector("#sobrenome").value;
     const idade = document.querySelector("#idade").value;
     const profissao = document.querySelector("#profissao").value;
-    spanResult.innerHTML = `BEM VINDO ${nome} ${sobrenome}, VOCÊ TEM ${idade} ANOS E TRABALHA COM ${profissao}`
-}
+
+    if (!nome && !sobrenome && !idade && !profissao) return result.innerHTML = `ERRO: Preencha todos os campos corretamente`
+    if (isNaN(idade)) return result.innerHTML = `ERRO: Coloque um número valido em Idade`
+
+    result.innerHTML = `BEM VINDO ${nome} ${sobrenome}, VOCÊ TEM ${idade} ANOS E TRABALHA COM ${profissao}`
+})
