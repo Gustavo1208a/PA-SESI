666fc0b22886da5b4c09d21e1fed095574cc07b4
Gustavo1208a <160923314+Gustavo1208a@users.noreply.github.com>
2026-06-17 10:30:27 -0300

    Connect Database

--- Aula 10 (PHP)/connection.php
+++ Aula 10 (PHP)/connection.php
@@ -6,4 +6,11 @@ $password = "root";
 $database = "aula_sesi";
 $port = 3307;
 
+try {
+    $conn = mysqli_connect($host, $user, $password, $database, $port);
+    echo "Conectado com o banco de dados MYSQL.";
+} catch (Exception $e) {
+    echo $e;
+}
+
 ?>
