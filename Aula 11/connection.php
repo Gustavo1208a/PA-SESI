749504cfbf6625f347e8ea39d451dc3e58444481
Gustavo1208a <160923314+Gustavo1208a@users.noreply.github.com>
2026-06-24 09:18:39 -0300

    Add connection.php for database connection setup

---
+++ Aula 11/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$password = "root";
+$database = "db_usuarios";
+$port = 3307;
+
+$conn = mysqli_connect($server, $user, $password, $database, $port);
+?>
