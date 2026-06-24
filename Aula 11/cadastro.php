adfda3188a10996a166a3aaafda4477f300f322a
Gustavo1208a <160923314+Gustavo1208a@users.noreply.github.com>
2026-06-24 09:16:59 -0300

    Update cadastro.php

--- Aula 11/cadastro.php
+++ Aula 11/cadastro.php
@@ -11,5 +11,9 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $pass = $_POST['senha'];
 
-mysqli_query($conn, "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$pass')");
+try {
+  mysqli_query($conn, "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$pass')");
+} catch (Exception $e) {
+    echo $e;
+}
 ?>
