7b64bb61860f7571edb772411144163acb3b7134
Gustavo1208a <160923314+Gustavo1208a@users.noreply.github.com>
2026-06-24 09:12:57 -0300

    Create index.php with a registration form

---
+++ Aula 11/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="cadastro.php" method="POST">
+        <input type="text" name="nome" placeholder="Digite seu nome ...">
+        <input type="text" name="email" placeholder="Digite seu email ...">
+        <input type="password" name="senha" placeholder="Digite sua senha ...">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
