66f4c06263c30bf168eb5efa9fab188780da5741
Gustavo1208a <160923314+Gustavo1208a@users.noreply.github.com>
2026-06-24 09:18:59 -0300

    Update cadastro.php

--- Aula 11/cadastro.php
+++ Aula 11/cadastro.php
@@ -1,11 +1,5 @@
 <?php 
-$server = "localhost";
-$user = "root";
-$password = "root";
-$database = "db_usuarios";
-$port = 3307;
-
-$conn = mysqli_connect($server, $user, $password, $database, $port);
+include "connection.php";
 
 $nome = $_POST['nome'];
 $email = $_POST['email'];
